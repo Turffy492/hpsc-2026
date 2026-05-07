6ecca42f146d2247b4e813b5024432d36a945df8
Turffy492 <sci.daffy@gmail.com>
2026-05-07 23:30:54 +0900

    Lecture4  homework

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,26 +1,54 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 16;
-  float x[N], y[N], m[N], fx[N], fy[N];
+  alignas(64) float x[N], y[N], m[N], fx[N], fy[N];
+
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+
+  __m512 xvec = _mm512_load_ps(x);
+  __m512 yvec = _mm512_load_ps(y);
+  __m512 mvec = _mm512_load_ps(m);
+  __m512 one = _mm512_set1_ps(1.0f);
+
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
+    __m512 xi = _mm512_set1_ps(x[i]);
+    __m512 yi = _mm512_set1_ps(y[i]);
+
+    __m512 rx = _mm512_sub_ps(xi, xvec);
+    __m512 ry = _mm512_sub_ps(yi, yvec);
+
+    __m512 r2 = _mm512_add_ps(
+      _mm512_mul_ps(rx, rx),
+      _mm512_mul_ps(ry, ry)
+    );
+
+    __mmask16 mask = ~(1 << i);
+
+    r2 = _mm512_mask_blend_ps(mask, one, r2);
+
+    __m512 inv_r = _mm512_rsqrt14_ps(r2);
+    __m512 inv_r3 = _mm512_mul_ps(
+      _mm512_mul_ps(inv_r, inv_r),
+      inv_r
+    );
+
+    __m512 coef = _mm512_mul_ps(mvec, inv_r3);
+
+    __m512 dfx = _mm512_mul_ps(rx, coef);
+    __m512 dfy = _mm512_mul_ps(ry, coef);
+
+    fx[i] -= _mm512_mask_reduce_add_ps(mask, dfx);
+    fy[i] -= _mm512_mask_reduce_add_ps(mask, dfy);
+
+    printf("%d %g %g\n", i, fx[i], fy[i]);
   }
 }
